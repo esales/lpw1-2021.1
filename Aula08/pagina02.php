afbc6e675d11f56309307e3e66a95b56689cdcc7
esales <ericsales0@gmail.com>
2021-05-24 20:54:40 -0300

    commit inicial

---
+++ Aula08/pagina02.php
@@ -0,0 +1,9 @@
+<?php
+
+ $nome = $_REQUEST['nome'];
+ $data = $_REQUEST['data'];
+ $area = $_REQUEST['area'];
+
+echo '<span style=color:lightblue>Nome: </span>' . $nome . '<br>';
+echo '<span style=color:lightblue>Data: </span>' . $data . '<br>';
+echo '<span style=color:lightblue>Área: </span>' . $area;
